50ab8329d2ec55bbbaf7ffdc3d24c45660f3ee86
Alphy <qassurance@protonmail.com>
2021-04-07 11:07:06 -0700

    Added TC-045

--- test/extended/gender.js
+++ test/extended/gender.js
@@ -13,6 +13,13 @@ describe('Gender field suite', function () {
             browser.refresh();
         });
 
+        it('TC-045 Button "it" is enabled', function () {
+            $$(sel.radioButtons)[gender.it].click();
+            let result = $(sel.btnIt).isSelected();
+            expect(result).toEqual(true);
+
+        });
+
         it('TC-047 User can choose only one button at the time: "she"', function () {
             $$(sel.radioButtons)[gender.she].click();
             let result = $$(sel.radioButtons)[gender.he].isSelected() && $$(sel.radioButtons)[gender.it].isSelected();
