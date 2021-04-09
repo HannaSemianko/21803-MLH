c04d1c1359b35ee67d4e77145bfbc3dae2ac5761
NataliaNazarova <natalya.i.nazarova@gmail.com>
2021-04-08 18:39:28 -0700

    fixed import methods

--- helpers/methods.js
+++ helpers/methods.js
@@ -1,7 +1,7 @@
 import sel from "../data/selectors";
 
 
-function inputValues4 (name, gender, age, story){
+function inputValues4(name, gender, age, story){
     $(sel.name).setValue(name);
     $$(sel.radioButtons)[gender].click();
     $(sel.age).setValue(age);
