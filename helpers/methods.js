902a2c0e7850a5ef66ce34a0900ff8f4d8a1ef75
NataliaNazarova <natalya.i.nazarova@gmail.com>
2021-04-09 23:44:19 -0700

    added TC-129 Story

--- helpers/methods.js
+++ helpers/methods.js
@@ -9,7 +9,7 @@ function inputValues4(name, gender, age, story){
     $$(sel.storyList)[story].click();
 }
 
-function inputValues5(name, gender, age, story){
+function inputValues4Submit(name, gender, age, story){
     $(sel.name).setValue(name);
     $$(sel.radioButtons)[gender].click();
     $(sel.age).setValue(age);
@@ -24,6 +24,50 @@ function clearBackspace (value){
     }
 }
 
+function getTitle (){
+    return $("h4").getText();
+}
+function textStory () {
+    return $$(".card-text")[0].getText();
+}
+
+function breakStoryIntoLines (){
+    let text = textStory();
+    let arrIncludesLines = text.split("\n");
+    console.log(arrIncludesLines)
+    return arrIncludesLines;
+}
+
+function getNameStory (){
+    let arrWordsOfLines1 = breakStoryIntoLines()[1].split(",");
+    return arrWordsOfLines1[0];
+}
+
+function getAgeStory () {
+    let arrWordsOfLines1 = breakStoryIntoLines()[1].split(" ");
+    return arrWordsOfLines1[3];
+}
+
+function getYears () {
+    let arrWordsOfLines1 = breakStoryIntoLines()[1].split(" ");
+    return arrWordsOfLines1[4];
+}
+
+function textWithoutLineBreak () {
+    let arrWords = textStory().replace(/\n/g, ' ').split(' ');
+    console.log(arrWords);
+    return arrWords;
+}
+
+function countGenderLower (genderTC) {
+    let arrWords = textWithoutLineBreak();
+    return arrWords.filter(el => el === genderTC).length;
+}
+
+function countPosGender (genderPosTC) {
+    let arrWords = textWithoutLineBreak();
+    return arrWords.filter(el => el === genderPosTC).length;
+}
 
 
-module.exports = {inputValues4, clearBackspace,inputValues5};
+module.exports = {inputValues4, clearBackspace,inputValues4Submit, getTitle, textStory, getNameStory, getAgeStory, getYears, countGenderLower, countPosGender};
